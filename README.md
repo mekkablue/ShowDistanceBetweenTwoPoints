d5a7e28697bf9f7bd1660136c6297f88007adcfc
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2016-01-23 12:48:08 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,37 @@
+# ShowDistanceBetweenTwoPoints.glyphsReporter
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert.
+It displays the distance between two selected nodes when exactly two points are selected, ignoring intersections in between.
+After installation, it will add the menu item *View > Show Distance between Two Points* (German: *Zeige Abstand zwischen zwei Punkten*, French: *Montre distance entre deux points*).
+You can set a keyboard shortcut in System Preferences.
+
+![Distance between two selected nodes.](ShowDistanceBetweenTwoPoints.png "Show Distance between Two Points Screenshot")
+
+### Installation
+
+1. Download the complete ZIP file and unpack it, or clone the repository.
+2. Double click the .glyphsReporter file. Confirm the dialog that appears in Glyphs.
+3. Restart Glyphs.
+
+### Usage Instructions
+
+1. Open a glyph in Edit view.
+2. Use *View > Show Distance between Two Points*.
+3. Select exactly two points (on-curve or off-curve).
+
+### Requirements
+
+The plugin needs Glyphs 2.3 or higher, running on OS X 10.9.1 or later. I can only test it in current OS versions, and I assume it will not work in prior versions.
+
+### License
+
+Copyright 2016 Rainer Erich Scheichelbauer (@mekkablue).
+Including sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ ShowDistanceBetweenTwoPoints.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+
+from GlyphsPlugins import *
+import math
+
+class ShowDistanceBetweenTwoPoints(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': 'Distance between Two Points',
+			'de': 'Abstand zwischen zwei Punkten', 
+			'fr': 'distance entre deux points'
+		})
+	
+	def roundDotForPoint( self, thisPoint, markerWidth ):
+		"""
+		Returns a circle with thisRadius around thisPoint.
+		"""
+		myRect = NSRect( ( thisPoint.x - markerWidth * 0.5, thisPoint.y - markerWidth * 0.5 ), ( markerWidth, markerWidth ) )
+		return NSBezierPath.bezierPathWithOvalInRect_(myRect)
+
+	def drawRoundedRectangleForStringAtPosition(self, thisString, center, fontsize):
+		scaledSize = fontsize / self.getScale()
+		width = len(thisString) * scaledSize * 0.7
+		rim = scaledSize * 0.3
+
+		panel = NSRect()
+		panel.origin = NSPoint( center.x-width/2, center.y-scaledSize/2-rim )
+		panel.size = NSSize( width, scaledSize + rim*2 )
+		
+		NSBezierPath.bezierPathWithRoundedRect_xRadius_yRadius_( panel, scaledSize*0.5, scaledSize*0.5 ).fill()
+		
+	
+	def foreground(self, layer):
+		currentSelection = layer.selection
+		if currentSelection and len(currentSelection) == 2:
+			node1 = currentSelection[0]
+			node2 = currentSelection[1]
+			if type(node1)==GSNode and type(node2)==GSNode:
+				# set drawing color:
+				fontColor = NSColor.whiteColor()
+				drawingColor = NSColor.grayColor()
+				drawingColor.set()
+				scale = self.getScale()
+				
+				# draw line:
+				NSBezierPath.setDefaultLineWidth_( 1.0 / scale )
+				NSBezierPath.strokeLineFromPoint_toPoint_( node1.position, node2.position )
+				
+				# draw handles:
+				# handlesize = self.getHandleSize() / scale
+				# dots = NSBezierPath.bezierPath()
+				# for thisNode in (node1,node2):
+				# 	newDot = self.roundDotForPoint( thisNode.position, handlesize )
+				# 	dots.appendBezierPath_( newDot )
+				# dots.stroke()
+				
+				# write measurement:
+				distance = math.hypot( node1.x - node2.x, node1.y - node2.y )
+				distanceString = "%.1f" % distance
+				middle = NSPoint( (node1.x+node2.x)*0.5, (node1.y+node2.y)*0.5 )
+				typeSize = 12.0
+				scaledSize = typeSize / scale
+				self.drawRoundedRectangleForStringAtPosition( distanceString, middle, typeSize )
+				self.drawTextAtPoint( distanceString, middle, fontColor=fontColor, align='center', fontSize=typeSize )
+
+
+		
